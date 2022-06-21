e957324e64b1df7aca6b0aaa7b5d0f78dd04d052
Bumblebee065 <92314744+Bumblebee065@users.noreply.github.com>
2022-06-21 13:45:55 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://35.89.39.122:9000//',
+  serverUrl: 'http://35.89.39.122:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
